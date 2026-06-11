da7ad5e956b67508e7672911fbcc1af182e10217
Rohit <95517751+rohitkumartyagi7010@users.noreply.github.com>
2026-06-11 15:08:27 +0530

    Add files via upload

---
+++ myfile.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+def addition(a,b):
+    return a + b
+num1 = int(input("enter your first number"))
+num2 = int(input("Enter your second number"))
+total = num1 +  num2
+print("Total of your numbers is :",total)
+
+
+# In[ ]:
+
+
+
+
